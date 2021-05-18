b548954e4963f320f0d5b8f9907d71c1a1bc8e9a
petrichor-002 <84283596+petrichor-002@users.noreply.github.com>
2021-05-19 02:09:19 +0530

    Add files via upload

--- patterns.c
+++ patterns.c
@@ -6,21 +6,21 @@ int main(){
     int rows;
     int i,j,k;
     pattern = get_int("Enter type of pattern (1 or 2) ");
-    rows = get_int("Number of rows");
-    if (pattern == 2){
-        for(i = 1;i<=rows; i++){
-            for(j = 1;j<=i; j++){
+    rows = get_int("Number of rows ");
+    if (pattern == 1){
+        for(i = 0;i<=rows; i++){
+            for(j = rows;j>i; j--){
                 printf("#");
             }
         printf("\n");
         }
     }
     else{
-        for(i = 1;i<=6;i++){
-            for(j = 6;j>i;j--){
+        for(i = 0;i<=rows;i++){
+            for(j = rows;j>i;j--){
                 printf(" ");
             }
-            for(k = 1;k <= j;k++)
+            for(k = 0;k <= j;k++)
             {
             printf("#");
             }
