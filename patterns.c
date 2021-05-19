48e7881a2431738d14fb18af6d29d7ab670d8037
petrichor-002 <84283596+petrichor-002@users.noreply.github.com>
2021-05-19 12:01:57 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -18,7 +18,7 @@ int main(){
     else{
         for(i = 0;i<rows;i++){
             for(j = rows;j>i;j--){
-                printf(" ");
+                printf("");
             }
             for(k = 0;k <= j;k++)
             {
